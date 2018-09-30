4f67dec85be17579674f1eb11fe82a9da2aa405e
kmaricha2 <keilerdavid2010@hotmail.com>
2018-09-30 03:15:43 -0500

    Initial commit

---
+++ app/src/main/java/com/devmasterteam/conversaodemoeda/MainActivity.java
@@ -0,0 +1,64 @@
+package com.devmasterteam.conversaodemoeda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Contém os elementos da tela em questão. Faz o carregamento uma única vez e pode ser usado a qualquer momento dentro da classe.
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Insere o layout na activity
+        setContentView(R.layout.activity_main);
+
+        // Busca os elementos da interface
+        this.mViewHolder.editReal = (EditText) this.findViewById(R.id.edit_real);
+        this.mViewHolder.textDollar = (TextView) this.findViewById(R.id.text_dollar);
+        this.mViewHolder.buttonCalculate = (Button) this.findViewById(R.id.button_calculate);
+
+        // Limpa os valores iniciais
+        this.clearValues();
+
+        // Adiciona evento de click no elemento
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        // Verifica se o elemento clicado é o que nos interessa
+        if (view.getId() == R.id.button_calculate) {
+
+            // Obtém o valor do EditText
+            Double mReal = Double.valueOf(this.mViewHolder.editReal.getText().toString());
+
+            // Converte valores
+            this.mViewHolder.textDollar.setText(String.format("%.2f", mReal * 0.00033613445));
+
+        }
+    }
+
+    /**
+     * Limpa os valores iniciais
+     */
+    private void clearValues() {
+        this.mViewHolder.textDollar.setText("");
+    }
+
+    /**
+     * Padrão ViewHolder
+     */
+    private static class ViewHolder {
+        private EditText editReal;
+        private TextView textDollar;
+        private Button buttonCalculate;
+    }
+}
